6c5738b2d1be62d1f729b1c903b393fba63f6f90
Pop228822 <465485@edu.itmo.ru>
2025-06-29 03:42:31 +0300

    first commit

---
+++ TrustStaffTests/TrustStaffTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrustStaffTests.swift
+//  TrustStaffTests
+//
+//  Created by Aram Gazaryan on 20.06.2025.
+//
+
+import Testing
+@testable import TrustStaff
+
+struct TrustStaffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
